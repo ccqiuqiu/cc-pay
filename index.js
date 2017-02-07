cb0572176282cacceb330b7eb58df4470f045947
xcc <111111>
2017-02-07 09:16:40 +0800

    d

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 /**
  * Created by cc on 2017/1/17.
  */
-var WxPay = require('./lib/wxpay')
-var AliPay = require('./lib/alipay')
+const WxPay = require('./lib/wxpay')
+const AliPay = require('./lib/alipay')
 
 module.exports = {
   WxPay: WxPay,
   AliPay: AliPay
 }
-
-let config = {
-  app_id: '2016073000127091',
-  private_key: './privateKey.txt',
-  public_key: './publicKey.txt',
-  dev: true,
-  notify_url: "http://localhost:8200"
-}
-let alipay = new AliPay(config)
-let re = alipay.getRequestURI({
-    biz_content: JSON.stringify({
-        body:  "支付测试",
-        subject: "支付测试",
-        out_trade_no: "201688888",
-        total_amount: 1.0,
-        product_code: "QUICK_WAP_PAY"
-    })
-});
-console.log(re)
