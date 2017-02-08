78586ae5c1af057032e81cd29aa3660239107e20
xcc <111111>
2017-02-08 14:55:13 +0800

    d

--- test.js
+++ test.js
@@ -11,15 +11,29 @@ let config = {
   notify_url: "http://localhost:8200"
 }
 let alipay = new AliPay(config)
+//网页支付
+/* re = alipay.getRequestURI({
+    biz_content: JSON.stringify({
+        body:  '支付测试',
+      subject: '支付测试',
+      out_trade_no: '201688888' + new Date().getTime(),
+      total_amount: 1.0,
+      product_code: "QUICK_WAP_PAY"
+    })
+})
+console.log(re) */
+//二维码
 alipay.getQrCode({
     biz_content: JSON.stringify({
-        body:  "支付测试",
-        subject: "支付测试",
-        out_trade_no: "2016888885677",
-        total_amount: 1.0
+        body:  '支付测试',
+      subject: '支付测试',
+      out_trade_no: '201688888' + new Date().getTime(),
+      total_amount: 1.0
     })
-}).then(function(re){
-      console.log(re.qr_code)
-    }).catch(function(err){
-    });
-// console.log(re)
+})
+.then(re => {
+  console.log(re);
+})
+.catch(error => {
+  console.log(re);
+})
